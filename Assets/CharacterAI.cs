243689e0730c7caeba1a61db6e48ab8543ed4193
Joshua Jaramillo <joshamyjarami@hotmail.com>
2021-01-28 17:28:12 -0700

    Customer Movement stuff
    pls help i dying

---
+++ Assets/CharacterAI.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAI : MonoBehaviour
+{
+    private GameObject centerTrigger;
+    private GameObject exitTrigger;
+    public float rate;
+    CharStats type;
+
+    //so that the customer knows when to leave
+    bool drinkAsked;
+    public bool drinkGiven;
+    public string nameOfDrink = "";
+    bool lostAndFoundAnswer;
+
+    public bool exit;
+    bool mid;
+
+    // Update is called once per frame
+    void Start()
+    {
+        type = this.gameObject.GetComponent<CharStats>();
+        drinkGiven = false;
+
+        centerTrigger = GameObject.Find("CenterTrigger").gameObject;
+        exitTrigger = GameObject.Find("ExitTrigger").gameObject;
+        
+        lostAndFoundAnswer = false;    
+        /*
+        If player wants lost and found item behind counter, show text bubble asking for it.
+        Wait for answer
+        */
+    }
+    private void Update()
+    {
+        //hit center?
+        if(Move(centerTrigger) == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, centerTrigger.transform.position, rate);
+        }
+        //stop
+        else if(Move(centerTrigger) == true && drinkAsked == false)
+        {
+            AskForDrink();
+            //check if want item
+            drinkAsked = true;
+        }
+        //after everything, move again
+        else if (Move(exitTrigger) == false && mid == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, exitTrigger.transform.position, rate);
+        }
+        //give ok to die
+        else if(Move(exitTrigger) == true)
+        {
+            exit = true;
+        }
+        /*If player is hit with bottle AND liar = true, answered = true, they move left
+        If player is handed an item AND liar = false, answered = true, they move left.
+        Else
+        Game Over prints on screen.*/
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //until drink given
+        if(gameObject.name == "Drink")
+        {
+            
+        }
+        //permission to move from mid
+        mid = true;
+    }
+    //hit center/exit yet?
+    bool Move(GameObject trigger)
+    {
+        if (transform.position.z < trigger.transform.position.z - .01 || transform.position.z > trigger.transform.position.z + .01)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    void AskForDrink()
+    {
+        //based on character rate, ask for drink
+        if (type.GenerateOrder() == "Ale")
+        {
+            //display ale drink text bubble
+            Debug.Log("Ale Pls");
+        }
+        else if (type.GenerateOrder() == "Wine")
+        {
+            //wine
+            Debug.Log("Wine pls");
+
+        }
+        else if (type.GenerateOrder() == "Water")
+        {
+            Debug.Log("Water pls");
+        }
+        
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //queue of customers
+    Queue<GameObject> line = new Queue<GameObject>();
+
+    //customer
+    public GameObject customer;
+
+    private void Start()
+    {
+        Instantiate(customer, new Vector3(13, 0, 6), Quaternion.identity);
+        customer.GetComponent<CharacterAI>().enabled = true;
+        line.Enqueue(customer);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //until the queue is full, keep spawning.
+        if(line.Count < 5)
+        {
+            //make a character, turn it off, put in line
+            Instantiate(customer, new Vector3(13, 0, 6), Quaternion.identity);
+            customer.GetComponent<CharacterAI>().enabled = false;
+            line.Enqueue(customer);
+        }
+        if(line.Peek().GetComponent<CharacterAI>().exit == true)
+        {
+            Destroy(line.Peek().gameObject);
+            line.Dequeue();
+        }
+    }
+}
