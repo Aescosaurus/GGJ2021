4f3c2adf0b00c9a44cf3244a56c985e99eb4e59f
Aescosaurus <michaelbuslik@hotmail.com>
2021-01-29 10:59:48 -0700

    Speech bubble start.

---
+++ Assets/Code/SpeechBubble.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		bubblePrefab = Resources.Load<GameObject>( "Prefabs/SpeechBubble" );
+
+		if( startingText.Length > 0 )
+		{
+			SpawnText( startingText );
+		}
+	}
+
+	public void SpawnText( string info )
+	{
+		Destroy( curBubble );
+		curBubble = Instantiate( bubblePrefab,transform );
+		curBubble.GetComponent<TextMesh>().text = info;
+	}
+	
+	[SerializeField] string startingText = "";
+
+	GameObject bubblePrefab;
+	GameObject curBubble;
+}
---
+++ Assets/Code/SpeechRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechRotate
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		cam = Camera.main;
+	}
+
+	void Update()
+	{
+		var rot = transform.eulerAngles;
+		var diff = cam.transform.position - transform.position;
+		rot.y = Mathf.Atan2( diff.x,diff.z ) * Mathf.Rad2Deg + 180.0f;
+		transform.eulerAngles = rot;
+	}
+
+	Camera cam;
+}
