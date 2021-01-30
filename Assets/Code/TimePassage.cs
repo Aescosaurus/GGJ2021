1effc8d09356cfb041fe0001f3a0dfd1d7c6d0fb
Kyle-Smart <ksmart@uccs.edu>
2021-01-29 21:34:29 -0700

    Got the day night cycle to detect when the day ends

--- Assets/Code/TimePassage.cs
+++ Assets/Code/TimePassage.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class TimePassage : MonoBehaviour
 {
+    //A value that tracks the day progress in the game.
+    static public int DAY = 1;
+
+    public bool endOfDay = false;
     //The sun 
     [SerializeField]
     GameObject theSun;
+    //The speed of the sun. This is how many degrees of rotation per second
+    [SerializeField]
+    int speedOfSun = 30;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +24,15 @@ public class TimePassage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theSun.transform.Rotate(Vector3.right * Time.deltaTime * 30);
+        //This roates the light source to make it look like a day night cycle
+        theSun.transform.Rotate(Vector3.right * Time.deltaTime * speedOfSun);
+
+        //Check if the sun is at a specific angle or passed it. If so set end of day to true and reset the suns position
+        if (theSun.transform.eulerAngles.x > 195)
+        {
+            endOfDay = true;
+            theSun.transform.Rotate(Vector3.right * Time.deltaTime * speedOfSun * 5);
+        }
+
     }
 }
