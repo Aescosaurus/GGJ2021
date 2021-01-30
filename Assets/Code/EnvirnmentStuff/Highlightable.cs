0c41ea485460307d1f935fcc3027f6c18b55bf95
Joshua Jaramillo <joshamyjarami@hotmail.com>
2021-01-29 19:22:26 -0700

    big highlighterboi

---
+++ Assets/Code/EnvirnmentStuff/Highlightable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlightable : MonoBehaviour
+{
+	Timer timer = new Timer(0.05f);
+    private void Start()
+    {
+		transform.GetChild(0).gameObject.SetActive(true);
+		transform.GetChild(1).gameObject.SetActive(false);
+	}
+    private void Update()
+    {
+        if(timer.Update(Time.deltaTime))
+        {
+			Unhighlight();
+        }
+    }
+    public void Highlight()
+	{
+		timer.Reset();
+		transform.GetChild(0).gameObject.SetActive(false);
+		transform.GetChild(1).gameObject.SetActive(true);
+	}
+	public void Unhighlight()
+	{
+		transform.GetChild(0).gameObject.SetActive(true);
+		transform.GetChild(1).gameObject.SetActive(false);
+	}
+}
