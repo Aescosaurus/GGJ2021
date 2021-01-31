3184a6853586846a97156ea84f2f2befe8c3f685
Aescosaurus <michaelbuslik@hotmail.com>
2021-01-31 12:16:08 -0700

    Pickup and throw fx.

---
+++ Assets/Code/SFXPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		audSrc = gameObject.AddComponent<AudioSource>();
+	}
+
+	public void PlaySFX( string path )
+	{
+		if( audSrc == null ) audSrc = gameObject.AddComponent<AudioSource>();
+		// if( path.Length > 0 )
+		{
+			if( !audCodex.ContainsKey( path ) )
+			{
+				audCodex.Add( path,Resources.Load<AudioClip>( "Audio/" + path ) );
+			}
+
+			audSrc.PlayOneShot( audCodex[path] );
+		}
+	}
+
+	AudioSource audSrc;
+	static Dictionary<string,AudioClip> audCodex = new Dictionary<string,AudioClip>();
+}
